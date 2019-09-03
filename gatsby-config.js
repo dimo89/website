000e1d0f0c8314f981ca22d067a4ce48a29424d5
dimo89 <dmitri.karasjov@gmail.com>
2019-09-03 13:51:10 +0300

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dmitri Karasjov`,
+    description: `Dmitri Karasjov`,
+    author: `Dmitri Karasjov`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -9,8 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my new site.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
